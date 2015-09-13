3dca3552d0c8fd4e182de03eec931475aa925929
nimayd <ntdesai2@illinios.edu>
2015-09-13 03:30:53 -0400

    Trying to parse xml

--- app/src/main/java/com/example/aditya/transitalarm2/MainActivity.java
+++ app/src/main/java/com/example/aditya/transitalarm2/MainActivity.java
@@ -30,7 +30,12 @@ public class MainActivity extends Activity {
 
     public static final OkHttpClient CLIENT = new OkHttpClient();
 
-    HashMap<String, double[]> stationInfo = null;
+    String data = "Bay Fair,37.69718,-122.126871:Pleasant Hill/Contra Costa Centre,37.92840,-122.056013:San Leandro,37.7226192,-122.1613112:Glen Park,37.73292,-122.434092:North Concord/Martinez,38.00327,-122.024597:Richmond,37.93688,-122.353165:16th St. Mission,37.76506,-122.419694:19th St. Oakland,37.8078,-122.269029:West Dublin/Pleasanton,37.69975,-121.928099:Hayward,37.67039,-122.087967:12th St. Oakland City Center,37.80366,-122.271604:Ashby,37.85302,-122.26978:MacArthur,37.82841,-122.267227:Castro Valley,37.69075,-122.075567:Fremont,37.55735,-121.9764:Embarcadero,37.79297,-122.396742:Walnut Creek,37.90562,-122.067423:West Oakland,37.8046747,-122.2945822:Balboa Park,37.7219808,-122.4474142:San Bruno,37.63775,-122.416038:Coliseum/Oakland Airport,37.75400,-122.197273:Millbrae,37.59978,-122.38666:Colma,37.68463,-122.466233:Downtown Berkeley,37.86986,-122.268045:South San Francisco,37.66417,-122.444116:El Cerrito del Norte,37.92565,-122.317269:Dublin/Pleasanton,37.70169,-121.900367:Lafayette,37.89339,-122.123801:Rockridge,37.84460,-122.251793:Oakland Int'l Airport,37.7129717,-122.21244024:Fruitvale,37.77496,-122.224274:South Hayward,37.6347995,-122.0575506:El Cerrito Plaza,37.903058,-122.2992715:Daly City,37.7061205,-122.4690807:24th St. Mission,37.75225,-122.418466:Concord,37.97373,-122.029095:Montgomery St.,37.78925,-122.401407:Union City,37.59120,-122.017867:Civic Center/UN Plaza,37.77952,-122.413756:San Francisco Int'l Airport,37.61603,-122.392612:Orinda,37.8783608,-122.1837911:Lake Merritt,37.79748,-122.265609:Powell St.,37.78499,-122.406857:North Berkeley,37.8740,-122.283451:Pittsburg/Bay Point,38.01891,-121.945154:";
+
+    String[][] data = new String[45][3];
+
+
+    //    HashMap<String, double[]> stationInfo = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,12 +65,13 @@ public class MainActivity extends Activity {
                 String str = o.toString();
                 //Toast.makeText(getBaseContext(), str, Toast.LENGTH_SHORT).show();
 
-                boolean[] coords = new boolean[2];
-                //coords[0] = 0;
-                //coords[1] = 0;
+                double[] coords = new double[2];
+                coords[0] = 37.000;
+                coords[1] = -122.123;
                 Intent intent = new Intent(getBaseContext(), DistanceActivity.class);
                 intent.putExtra("stationName", str);
-                //intent.putExtra("stationCoords", []);
+                intent.putExtra("stationCoords", coords);
+                startActivity(intent);
             }
         });
 
@@ -84,21 +90,17 @@ public class MainActivity extends Activity {
 
             @Override
             public void onResponse(Response response) throws IOException {
-                Log.d(TAG, "Got response: " + response.body().string());
+                //Log.d(TAG, "Got response: " + response.body().string());
                 stationInfoParser obj = new stationInfoParser();
-//                HashMap<String, double[]> stationInfo = null;
+                HashMap<String, double[]> stationInfo = null;
                 try {
-                    stationInfo = obj.parseForCoords(response.body().byteStream());
+                    stationInfo = obj.parseForCoords(response.body().string());
+                    System.out.println(stationInfo.keySet());
                 } catch (XmlPullParserException e) {
                     e.printStackTrace();
                 }
 
-                Iterator it = stationInfo.entrySet().iterator();
-                while (it.hasNext()) {
-                    HashMap.Entry pair = (HashMap.Entry)it.next();
-                    System.out.println(pair.getKey() + " = " + pair.getValue());
-                    it.remove(); // avoids a ConcurrentModificationException
-                }
+
             }
         });
     }
--- app/src/main/java/com/example/aditya/transitalarm2/stationInfoParser.java
+++ app/src/main/java/com/example/aditya/transitalarm2/stationInfoParser.java
@@ -3,11 +3,13 @@
  */
 package com.example.aditya.transitalarm2;
 
+import java.io.StringReader;
 import java.util.HashMap;
 import java.io.IOException;
 import java.io.InputStream;
 
 import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 import org.xmlpull.v1.XmlPullParserException;
 import android.util.Xml;
 
@@ -15,32 +17,34 @@ import android.util.Log;
 
 
 public class stationInfoParser {
-    public HashMap<String, double[]> parseForCoords(InputStream in) throws XmlPullParserException{
-        XmlPullParser parser = Xml.newPullParser();
-        parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-        parser.setInput(in, null);
+    public HashMap<String, double[]> parseForCoords(String in) throws XmlPullParserException{
+        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+        XmlPullParser parser = factory.newPullParser();
+//        parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+        parser.setInput(new StringReader(in));
         HashMap<String, double[]> stations = new HashMap<>(); // station -> [lat,long]
         String currentTag = null;
         String currStation = null; //Used as key of stations.
 
         int eventType = parser.getEventType();
+        Log.d("fuck", in);
         while (eventType != XmlPullParser.END_DOCUMENT) {
             if (eventType == XmlPullParser.START_TAG) {
                 currentTag = parser.getName();
             }
             else if (eventType == XmlPullParser.TEXT) {
-                if ("name".equals(currentTag)) {
+                if (("name").equals(currentTag)) {
                     currStation = currentTag;
                     Log.d("stationInfoParser", currStation);
                     stations.get(currStation);
                     double[] coords = new double[2];
                     stations.put(currStation, coords);
                 }
-                if ("gtfs_latitude".equals(currentTag)) {
+                if (("gtfs_latitude").equals(currentTag)) {
                     stations.get(currStation)[0] = Double.valueOf(parser.getText());
                     // Can insert error checking for null.
                 }
-                if ("gtfs_longitude".equals(currentTag)) {
+                if (("gtfs_longitude").equals(currentTag)) {
                     stations.get(currStation)[1] = Double.valueOf(parser.getText());
                     // Can insert error checking for null.
                 }
@@ -49,7 +53,13 @@ public class stationInfoParser {
                 continue;
             }
             try {
-                eventType = parser.next();
+                eventType = parser.nextToken();
+//                System.out.println("START_DOCUMENT" + XmlPullParser.START_DOCUMENT);
+//                System.out.println("TEXT" + XmlPullParser.TEXT);
+//                System.out.println("END_TAG" + XmlPullParser.END_TAG);
+//                System.out.println("START_TAG" + XmlPullParser.START_TAG);
+//                System.out.println("END_DOCUMENT" + XmlPullParser.END_DOCUMENT);
+
             } catch(IOException i){
                 stations.get(currStation)[0]=0;
                 stations.get(currStation)[1]=0;
