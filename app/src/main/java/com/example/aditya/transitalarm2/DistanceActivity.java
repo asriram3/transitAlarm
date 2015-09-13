521738843dd6167b9ae2af9a21aac19a04670051
unknown <manikum2@illinois.edu>
2015-09-12 22:16:01 -0500

    very few changes

--- app/src/main/java/com/example/aditya/transitalarm2/DistanceActivity.java
+++ app/src/main/java/com/example/aditya/transitalarm2/DistanceActivity.java
@@ -5,23 +5,21 @@ import android.content.Intent;
 import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 
-public class DistanceActivity extends ActionBarActivity {
+public class DistanceActivity extends AppCompatActivity {
 
     String stationname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //get intent
-        Intent intent = getIntent();
-        //get station data from intent
-        //stationname = intent.getStringExtra();
         double[] stationCoords;
+        //collecting extra data from intent
         Bundle stationData = getIntent().getExtras();
         stationname=stationData.getString("stationName");
         stationCoords=stationData.getDoubleArray("stationCoords");
@@ -35,9 +33,6 @@ public class DistanceActivity extends ActionBarActivity {
         PendingIntent pi = PendingIntent.getService(this, 1, new Intent(this, intentService.class), 0);
         locationManager.addProximityAlert(stationCoords[0], stationCoords[1], 3220, -1, pi);
     }
-
-    //public double[] getCurrentCoords(){}
-    //NEED TO IMPLEMENT ^^^^^^^^^^^^^^^^
 
     public double getDistance(double[] stationCoordinates, double[] currentCoordinates){
         return Math.sqrt((Math.pow(stationCoordinates[0],2)-(Math.pow(currentCoordinates[0],2))) + (Math.pow(stationCoordinates[1],2)-(Math.pow(currentCoordinates[1],2))));
