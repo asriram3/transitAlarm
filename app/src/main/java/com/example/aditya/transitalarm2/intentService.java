feb1e30123677cbb2330b72ac0965ebec496346d
unknown <manikum2@illinois.edu>
2015-09-13 06:47:31 -0500

    temp notification test build

--- app/src/main/java/com/example/aditya/transitalarm2/intentService.java
+++ app/src/main/java/com/example/aditya/transitalarm2/intentService.java
@@ -1,10 +1,12 @@
 package com.example.aditya.transitalarm2;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 //put service into manifest
 /**
  * Created by Kasi on 9/12/2015.
@@ -19,6 +21,14 @@ public class intentService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        //temp for notifications
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setContentTitle("WAKE UP!");
+        builder.setContentText("You are near your stop!");
+        NotificationManager nf = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        nf.notify(1, builder.build());
+        //end temp
+
         System.out.println("Timer is on now!");
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         Bundle stationData = intent.getExtras();
